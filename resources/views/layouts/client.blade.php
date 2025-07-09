f98b005ca2b693f930f9999ef0ea49abe6c47818
AssoDePicche <samuelprado730@gmail.com>
2025-07-09 14:13:45 -0300

    navbar

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -1,15 +1,20 @@
 @extends('layouts.app')
 
 @section('navbar')
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name') }}</a>
-        <div class="collapse navbar-collapse">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="">Categorias</a></li>
-                <li class="nav-item"><a class="nav-link" href="">Produtos</a></li>
-            </ul>
-        </div>
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark mb-4">
+  <div class="container-fluid">
+    <a class="navbar-brand text-bold" href="/dashboard">{{ config('app.name') }}</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="/dashboard">Dashboard</a>
+        <a class="nav-link" href="/categories">Categorias</a>
+        <a class="nav-link" href="/products">Productos</a>
+        <a class="nav-link" href="/transactions">Transações</a>
+      </div>
     </div>
+  </div>
 </nav>
 @endsection
